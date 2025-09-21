54540160ad560998da4e3a64a2a8c4159a86f6c8
ameizstanley <stanleyameiz@gmail.com>
2025-09-21 03:59:38 +0100

    week 3 assignment

---
+++ database/assignment2.sql
@@ -0,0 +1,12 @@
+UPDATE inventory
+SET inv_description = REPLACE (inv_description, 'small interiors', 'a huge interior')
+WHERE inv_make = 'GM' AND inv_model = 'Hummer'
+
+SELECT i.inv_make, i.inv_model, c.classification_name
+FROM inventory i
+INNER JOIN classification c ON i.classification_id = c.classification_id
+WHERE c.classification_name = 'Sport';
+
+UPDATE inventory
+SET inv_image = REPLACE(inv_image, '/images/', '/images/vehicles/'),
+    inv_thumbnail = REPLACE(inv_thumbnail, '/images/', '/images/vehicles/');
